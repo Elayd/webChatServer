20af38999e5cdda11db9d64c85b6883fc65a2557
Dmitrii Sakulin <elaydweb@gmail.com>
2024-10-04 22:19:43 +0300

    fix after review

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { config } from '../config'
 export const protect = (req, res, next) => {
-    const token = req.headers['authorization']
-    if (!token) {
+    const authHeader = req.headers['authorization']
+    if (!authHeader) {
         return res.status(400).json({ message: 'No token' })
     }
     try {
+        const token = authHeader.split(' ')[1]
         jwt.verify(token, config.JWT_SECRET)
         next()
     } catch {
