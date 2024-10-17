abceb6eb92e0fd14ed71b7789abe8a945eda121e
Dmitrii Sakulin <elaydweb@gmail.com>
2024-10-17 22:38:09 +0300

    Part of fixes v2

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,22 @@
 import mongoose from 'mongoose'
 
+interface User {
+    typeAuth: 'google' | 'common'
+    email: string
+    password: string
+    firstName: string
+    secondName: string
+    fullName: string
+    picture: string
+}
+
 const userSchema = new mongoose.Schema({
     typeAuth: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: {
         type: String,
         select: true,
-        required: function () {
+        required: function (this: User) {
             return this.typeAuth !== 'google'
         }
     },
