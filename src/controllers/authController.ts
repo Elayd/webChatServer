f535e02256a7de63ad9ec936d5fad21671d122c1
Dmitrii Sakulin <elaydweb@gmail.com>
2024-10-22 23:08:22 +0300

    Add zod

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,9 @@ import User from '../models/user'
 import bcrypto from 'bcryptjs'
 import { handleTokens } from '../helpers/createTokens'
 import { Request, Response } from 'express'
+import { UserAuthSchema } from '../schemas/authSchema'
+import { zodErrorsMapper } from '../helpers/zodErrorsMapper'
+import { z } from 'zod'
 
 interface AuthRequest extends Request {
     body: {
@@ -11,19 +14,29 @@ interface AuthRequest extends Request {
 }
 export const authController = async (req: AuthRequest, res: Response) => {
     const { email, password } = req.body
-    const user = await User.findOne({ email: email })
 
-    if (!user) return res.status(401).json({ error: "User Doesn't Exist" })
+    try {
+        const validatedData = UserAuthSchema.parse({ email, password })
+        const user = await User.findOne({ email: validatedData.email })
 
-    if (!user.password) {
-        return res.status(400).json({ message: 'Invalid credentials' })
+        if (!user) return res.status(401).json({ error: "User Doesn't Exist" }) // here will place error: {code: forExample 100, which mean need to show error validation}
+
+        if (!user.password) {
+            return res.status(400).json({ message: 'Invalid credentials' }) // same
+        }
+
+        const matchedPassword = await bcrypto.compare(validatedData.password, user.password)
+
+        if (!matchedPassword) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+
+        return handleTokens(res, user._id)
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            const errors = zodErrorsMapper<keyof AuthRequest['body']>(error.formErrors.fieldErrors)
+            console.log(errors, 'erros')
+            res.status(400).json({ errors })
+        }
     }
-
-    const matchedPassword = await bcrypto.compare(password, user.password)
-
-    if (!matchedPassword) {
-        return res.status(400).json({ message: 'Invalid credentials' })
-    }
-
-    return handleTokens(res, user._id)
 }
--- src/controllers/regController.ts
+++ src/controllers/regController.ts
@@ -2,6 +2,9 @@ import { handleTokens } from '../helpers/createTokens'
 import User from '../models/user'
 import bcrypt from 'bcryptjs'
 import { Request, Response } from 'express'
+import { UserRegistrationSchema } from '../schemas/registrationSchema'
+import { zodErrorsMapper } from '../helpers/zodErrorsMapper'
+import { z } from 'zod'
 
 interface RegRequest extends Request {
     body: {
@@ -13,23 +16,32 @@ interface RegRequest extends Request {
 export const registration = async (req: RegRequest, res: Response) => {
     const { email, password } = req.body
 
-    const user = await User.findOne({ email })
+    try {
+        const validatedData = UserRegistrationSchema.parse({ email, password })
 
-    if (!user) {
-        bcrypt.hash(password, 10).then((hash) => {
-            const user = User.create({
-                email: email,
-                password: hash,
-                typeAuth: 'common'
+        const user = await User.findOne({ email: validatedData.email })
+
+        if (!user) {
+            bcrypt.hash(validatedData.password, 10).then((hash) => {
+                const user = User.create({
+                    email: validatedData.email,
+                    password: hash,
+                    typeAuth: 'common'
+                })
+                    .then(() => {
+                        return handleTokens(res, user._id)
+                    })
+                    .catch((err) => {
+                        if (err) {
+                            res.status(400).json({ error: err })
+                        }
+                    })
             })
-                .then(() => {
-                    return handleTokens(res, user._id)
-                })
-                .catch((err) => {
-                    if (err) {
-                        res.status(400).json({ error: err })
-                    }
-                })
-        })
+        }
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            const errors = zodErrorsMapper<keyof RegRequest['body']>(error.formErrors.fieldErrors)
+            res.status(400).json({ errors })
+        }
     }
 }
